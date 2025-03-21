882998ca28aa51dd359533bfd167738e541b7cd4
Paphitis <81754722+Paphitis05@users.noreply.github.com>
2025-03-21 15:37:40 +0200

    Add files via upload

--- KoumbariFit/DjangoProject/DjangoProject/settings.py
+++ KoumbariFit/DjangoProject/DjangoProject/settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -27,11 +25,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
