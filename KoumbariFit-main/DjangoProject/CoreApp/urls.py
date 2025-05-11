d42a871c7bd0c5535f880570f4070d841577a1e1
Paphitis <81754722+Paphitis05@users.noreply.github.com>
2025-05-11 23:34:24 +0300

    Add files via upload

---
+++ KoumbariFit-main/DjangoProject/CoreApp/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from CoreApp import views  # Import views from your app
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls, name='admin'),  # Admin URL
+    path('', views.home, name='home'),  # Home page for login
+    path('register/', views.register, name='register'),  # Registration page
+    path('Feed/', views.Feed, name='Feed'),  # Feed page
+    path('User_Profile/', views.User_Profile, name='User_Profile'),  # User profile page
+    path('edit_profile/', views.edit_profile, name='edit_profile'),  # Edit profile page(partial HTMX)
+    path('logout/', views.user_logout, name='logout'), # Log out Function Redirecting to the login page
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
